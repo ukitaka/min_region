a1b4a1bc4c4061fcbc669791298811d46669ee22
ukitaka <yuki.takahashi.1126@gmail.com>
2017-08-25 17:56:37 +0900

    Impl `generateMain`

--- src/codegen/llvm/generator.rs
+++ src/codegen/llvm/generator.rs
@@ -4,7 +4,11 @@ pub struct LLVMIRGenerator;
 
 impl LLVMIRGenerator {
     fn generateInMain(expr: Expr) -> String {
-        String::from("main")
+        format!("\
+define i32 @main(i32, i8**) #0 {{
+    ret i32 1
+}}\
+        ")
     }
 
     pub fn generate(expr: Expr) -> String {
